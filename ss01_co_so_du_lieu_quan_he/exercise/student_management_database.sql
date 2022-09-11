d76fb381bb613fdfcc29daa929e38c41df1898ad
ledat0711 <leanhdat1994@gmail.com>
2022-09-11 23:06:56 +0700

    Send

---
+++ ss01_co_so_du_lieu_quan_he/exercise/student_management_database.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS student_management;
+CREATE DATABASE student_management;
+
+USE student_management;
+
+CREATE TABLE student (
+    `ID` INT PRIMARY KEY,
+    `Name` VARCHAR(45),
+    `Age` INT,
+    `Country` VARCHAR(45)
+);
+    
+CREATE TABLE class (
+    `ID` INT PRIMARY KEY,
+    `Name` VARCHAR(45) NULL
+);
+
+CREATE TABLE teacher (
+    `ID` INT PRIMARY KEY,
+    `Name` VARCHAR(45),
+    `Age` INT,
+    `Country` VARCHAR(45)
+);
+
+INSERT INTO student(id, name, age, country)
+VALUES (001, "Lê Anh Đạt", "28","Việt Nam");
