c623867ed6a4ca1843205162b9eb515f522b161a
ledat0711 <leanhdat1994@gmail.com>
2022-09-13 20:57:44 +0700

    Send

--- ss02_thiet_ke_va_tao_csdl/exercise/bai_tap_1/erd_sang_mh_quan_he.sql
+++ ss02_thiet_ke_va_tao_csdl/exercise/bai_tap_1/erd_sang_mh_quan_he.sql
@@ -1,4 +1,49 @@
-DROP TABLE IF EXISTS erd_sang_mh_quan_he;
-CREATE TABLE erd_sang_mh_quan_he;
+DROP DATABASE IF EXISTS erd_sang_mh_quan_he;
+CREATE DATABASE erd_sang_mh_quan_he;
 USE erd_sang_mh_quan_he;
 
+CREATE TABLE phieu_xuat(
+	so_phieu_xuat INT PRIMARY KEY,
+	ngay_xuat DATE
+);
+
+CREATE TABLE vat_tu(
+	ma_vat_tu INT PRIMARY KEY,
+    ten_vat_tu VARCHAR(55)
+);
+
+CREATE TABLE chi_tiet_phieu_xuat(
+    so_phieu_xuat INT,
+    ma_vat_tu INT,
+	don_gia_xuat DOUBLE,
+    so_luong_xuat INT,
+    PRIMARY KEY (so_phieu_xuat,ma_vat_tu),
+    FOREIGN KEY (so_phieu_xuat) REFERENCES phieu_xuat (so_phieu_xuat),
+	FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu (ma_vat_tu)
+);
+
+CREATE TABLE phieu_nhap(
+	so_phieu_nhap INT PRIMARY KEY,
+    ngay_nhap DATE
+);
+
+CREATE TABLE chi_tiet_phieu_nhap(
+	ma_vat_tu INT,
+    so_phieu_nhap INT,
+    don_gia_nhap DOUBLE,
+    so_luong_nhap INT,
+    PRIMARY KEY (ma_vat_tu,so_phieu_nhap),
+    FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
+    FOREIGN KEY (so_phieu_nhap) REFERENCES phieu_nhap(so_phieu_nhap)
+);
+
+CREATE TABLE don_dat_hang(
+	ma_so_dat_hang INT PRIMARY KEY,
+    ngay_dat_hang DATE
+);
+
+CREATE TABLE chi_tiet_don_dat_hang(
+	so_don_hang INT,
+    ma_vat_tu INT,
+    
+);
