19d24268a79fa5fadd88f1926b642c9c24798652
ledat0711 <leanhdat1994@gmail.com>
2022-09-15 20:53:40 +0700

    Send

--- ss04_cac_ham_thong_dung_trong_sql/exercise/bai_tap_1_ss04.sql
+++ ss04_cac_ham_thong_dung_trong_sql/exercise/bai_tap_1_ss04.sql
@@ -2,14 +2,14 @@ USE csdl_sinh_vien;
 
 -- 1. Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 SELECT *
-FROM subject sub
+FROM `subject` sub
 WHERE sub.credit = (SELECT MAX(credit) FROM `subject`);
             
 -- 2.Hiển thị các thông tin môn học có điểm thi lớn nhất.
 SELECT sub.*, m.mark
 FROM `subject` sub
 JOIN mark m ON m.sub_id = sub.sub_id
-WHERE m.mark = (SELECT MAX(m.mark));
+WHERE m.mark = (SELECT MAX(mark) from mark);
 
 -- 3. Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 SELECT s.*,AVG(mark.mark) AS diem_trung_binh
