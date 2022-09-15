f219f9684f4144c9ac446d81271c17b705cd1a09
ledat0711 <leanhdat1994@gmail.com>
2022-09-15 14:34:23 +0700

    Send

--- ss04_cac_ham_thong_dung_trong_sql/exercise/bai_tap_1_ss04.sql
+++ ss04_cac_ham_thong_dung_trong_sql/exercise/bai_tap_1_ss04.sql
@@ -1,8 +1,19 @@
 USE csdl_sinh_vien;
-SELECT address, COUNT(student_id) AS so_luong_hoc_vien
-FROM student
-GROUP BY address;
 
-SELECT s.student_id, s.student_name, AVG(mark)
-FROM student s JOIN mark m on s.student_id = m.student_id
-GROUP BY s.student_id, s.student_name;
+-- 1. Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+SELECT * FROM `subject` 
+order by credit desc
+limit 1;
+
+-- 2.Hiển thị các thông tin môn học có điểm thi lớn nhất.
+SELECT sub.*, mark.mark FROM `subject` sub
+JOIN mark ON mark.sub_id= sub.sub_id
+order by mark desc
+limit 1;
+
+-- 3. Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+SELECT s.*, IFNULL((m.mark),0) AS diem_trung_binh
+FROM student s 
+LEFT JOIN mark m ON m.student_id = s.student_id
+GROUP BY m.student_id
+ORDER BY diem_trung_binh DESC;
