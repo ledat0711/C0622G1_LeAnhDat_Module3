faa6b5a21af37175edc7566dac89016c34ed9215
ledat0711 <leanhdat1994@gmail.com>
2022-09-15 10:01:12 +0700

    Send

--- furama_sql/yeu_cau_2_den_5.sql
+++ furama_sql/yeu_cau_2_den_5.sql
@@ -2,19 +2,19 @@ USE furama_database;
 
 -- 2. Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” 
 -- và có tối đa 15 kí tự.
-SELECT * FROM nhan_vien
-WHERE char_length(ho_ten) <= 15 AND ho_ten REGEXP '^[HKT].*$';
+SELECT nv.ma_nhan_vien,nv.ho_ten FROM nhan_vien nv
+WHERE char_length(ho_ten) <= 15 AND ho_ten REGEXP '^[HTK].*$';
 
 -- 3. Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi 
 -- và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
-SELECT * FROM khach_hang
+SELECT kh.ma_khach_hang,kh.ho_ten FROM khach_hang kh
 WHERE (TIMESTAMPDIFF(YEAR, ngay_sinh, DATE(NOW())) BETWEEN 18 AND 50)
 AND dia_chi REGEXP '^.*(Đà Nẵng|Quảng Trị)$';
 
 -- 4. Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
 -- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
 -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
-SELECT *, COUNT(hd.ma_khach_hang) AS so_lan_dat_phong
+SELECT kh.ma_khach_hang,kh.ho_ten, COUNT(hd.ma_khach_hang) AS so_lan_dat_phong
 FROM loai_khach lk
 JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
 JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
