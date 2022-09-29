06946d9aa2173a8a3c5b0aa9bf66c9532537affb
ledat0711 <leanhdat1994@gmail.com>
2022-09-30 01:03:17 +0700

    Send

--- ss10_jsp_and_jstl/exercise/display-customer-list/src/main/java/CustomerServlet.java
+++ ss10_jsp_and_jstl/exercise/display-customer-list/src/main/java/CustomerServlet.java
@@ -18,11 +18,11 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1994-11-07"), "Quảng Nam", "pictures/human_2.jpg"));
-        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1994-11-07"), "Quảng Nam", "pictures/human_1.jpg"));
-        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1994-11-07"), "Quảng Nam", "pictures/human_3.jpg"));
-        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1994-11-07"), "Quảng Nam", "pictures/human_4.jpg"));
-        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1994-11-07"), "Quảng Nam", "pictures/human_5.jpg"));
+        customerList.add(new Customer("Nguyễn A", LocalDate.parse("1995-08-09"), "Quảng Nam", "pictures/human_2.jpg"));
+        customerList.add(new Customer("Nguyễn B", LocalDate.parse("1999-07-05"), "Quảng Nam", "pictures/human_1.jpg"));
+        customerList.add(new Customer("Nguyễn V", LocalDate.parse("1998-10-09"), "Quảng Nam", "pictures/human_3.jpg"));
+        customerList.add(new Customer("Nguyễn C", LocalDate.parse("2000-12-01"), "Quảng Nam", "pictures/human_4.jpg"));
+        customerList.add(new Customer("Nguyễn D", LocalDate.parse("1997-04-09"), "Quảng Nam", "pictures/human_5.jpg"));
     }
 
     @Override
